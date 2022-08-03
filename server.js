a1137aa9e71c5e3ac78cedb087afdcca23fd3f7e
fheezy <farhanhoque764@yahoo.com>
2022-08-03 17:48:44 -0500

    created a server.js file and installed all the dependecies

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+
+const rooms = ['Public Room'];
+const cors = require('cors');
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(cors());
+
+//communication with the database
+const server = require('http').createServer(app);
+const PORT = 5001;
+const io = require('socket.io')(server, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST']
+  }
+})
+
+
+server.listen(PORT, ()=> {
+    console.log('listening to port', PORT)
+})
